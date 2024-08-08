d8e2d76128514f797e207d823aaf0c37f9de1d73
headlessNode <harriskhan047@outlook.com>
2024-08-08 17:59:26 +0500

    add top discount banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<div className="top-bar px-5 py-1 font-montserrat font-medium text-xs bg-blue flex justify-between md:justify-start md:gap-20 md:text-sm">
+				<a href="#" className="text-white text-center uppercase hover:underline">
+					get upto 65% off
+				</a>
+				<a href="#" className="text-white text-center uppercase hover:underline">
+					free shipping on orders $50+
+				</a>
+				<a href="#" className="text-white text-center uppercase hover:underline">
+					get your $20 bonus reward
+				</a>
+			</div>
+			<div className="wrapper"></div>
+		</>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,jsx}'],
 	theme: {
+		screens: {
+			md: '820px',
+		},
+		colors: {
+			blue: '#3D58B8',
+			white: '#FFFFFF',
+		},
+		fontFamily: {
+			montserrat: ['Montserrat', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
