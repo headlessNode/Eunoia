37426fde55c686d3751217ada93cf42494d4f11d
headlessNode <harriskhan047@outlook.com>
2024-08-10 18:13:38 +0500

    Add marquee animation to top bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
+import TopBar from './components/TopBar';
+
 function App() {
 	return (
 		<>
-			<div className="top-bar px-5 py-1 font-montserrat font-medium text-xs bg-blue flex justify-between md:justify-start md:gap-20 md:text-sm">
-				<a href="#" className="text-white text-center uppercase hover:underline">
-					get upto 65% off
-				</a>
-				<a href="#" className="text-white text-center uppercase hover:underline">
-					free shipping on orders $50+
-				</a>
-				<a href="#" className="text-white text-center uppercase hover:underline">
-					get your $20 bonus reward
-				</a>
-			</div>
+			<TopBar />
 			<div className="wrapper"></div>
 		</>
 	);
