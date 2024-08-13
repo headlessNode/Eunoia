c395614b99c89da9e5b3fd68e8e873d2d2e3bda9
headlessNode <harriskhan047@outlook.com>
2024-08-13 12:51:30 +0500

    Add menu animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+import { useGSAP } from '@gsap/react';
 
 export default function Header() {
 	const [isMenuActive, setIsMenuActive] = useState(false);
@@ -16,6 +15,42 @@ export default function Header() {
 			setIsMenuActive(true);
 		}
 	}
+
+	function menuAnimation(scale, pathOne, pathTwo) {
+		const tl = gsap
+			.timeline()
+			.to(menu.current, {
+				transformOrigin: 'top center',
+				scaleY: scale,
+				ease: 'power1.in',
+				duration: 0.3,
+			})
+			.to(
+				'.menu-icon .svg-line-one',
+				{
+					attr: pathOne,
+					duration: 0.3,
+					ease: 'linear',
+				},
+				'<'
+			)
+			.to(
+				'.menu-icon .svg-line-two',
+				{
+					attr: pathTwo,
+					duration: 0.3,
+					ease: 'linear',
+				},
+				'<'
+			);
+		return tl;
+	}
+
+	useGSAP(() => {
+		isMenuActive
+			? menuAnimation(1, { d: 'M10 30 L50 70' }, { d: 'M10 70 L50 30' })
+			: menuAnimation(0, { d: 'M10 40 L75 40' }, { d: 'M10 60 L75 60' });
+	}, [isMenuActive]);
 
 	return (
 		<div ref={headerContainer} className="header-container px-3 py-5 w-full flex flex-col">
@@ -43,11 +78,27 @@ export default function Header() {
 				</div>
 				<div className="menu-icon lg:hidden" onClick={showMenu}>
 					<svg width="40" height="40" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10 30h80M10 60h80" stroke="#000" strokeWidth="8" strokeLinecap="round" />
+						<path
+							className="svg-line-one"
+							d="M10 40 L75 40"
+							stroke="#000"
+							strokeWidth="8"
+							strokeLinecap="round"
+						/>
+						<path
+							className="svg-line-two"
+							stroke="#000"
+							strokeWidth="8"
+							strokeLinecap="round"
+							d="M10 60 L75 60"
+						/>
 					</svg>
 				</div>
 			</nav>
-			<div ref={menu} className={`menu relative ${isMenuActive ? 'flex flex-col gap-2' : 'hidden'}`}>
+			<div
+				ref={menu}
+				className={`menu py-5 font-medium relative ${isMenuActive ? 'flex flex-col gap-2' : 'hidden'}`}
+			>
 				<NavLink className="uppercase hover:underline">shop</NavLink>
 				<NavLink className="uppercase hover:underline">men</NavLink>
 				<NavLink className="uppercase hover:underline">women</NavLink>
