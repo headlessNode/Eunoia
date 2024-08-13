2c8e0021d7a2d849b46d8caa4278aa0d1bdbeb8e
headlessNode <harriskhan047@outlook.com>
2024-08-13 19:30:18 +0500

    Add initial hero section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import TopBar from './components/TopBar';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 export default function HomePage() {
 	return (
@@ -7,6 +8,7 @@ export default function HomePage() {
 			<TopBar />
 			<div className="wrapper max-w-8xl w-full border-2 h-screen">
 				<Header />
+				<Hero />
 			</div>
 		</div>
 	);
