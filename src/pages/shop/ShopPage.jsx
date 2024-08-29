4b74e0803012089dffec6a863c208288be84aec9
headlessNode <harriskhan047@outlook.com>
2024-08-29 15:22:14 +0500

    feat: add shop page

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -2,12 +2,14 @@ import Header from '../components/Header';
 import MainBody from './components/MainBody';
 import NewsLetter from '../components/NewsLetter';
 import Footer from '../components/Footer';
+import { useLoaderData } from 'react-router-dom';
 
 export default function ShopPage() {
+	const data = useLoaderData();
 	return (
 		<div>
 			<Header />
-			<MainBody />
+			<MainBody products={data} />
 			<NewsLetter />
 			<Footer />
 		</div>
