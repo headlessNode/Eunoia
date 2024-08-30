d5ca9a9d37c157a5293f35e5ad06c5c4ca1a77bd
headlessNode <harriskhan047@outlook.com>
2024-08-30 21:44:01 +0500

    feat: add initial mens page

--- src/pages/categories/MensPage.jsx
+++ src/pages/categories/MensPage.jsx
@@ -1,3 +1,17 @@
+import Header from '../components/Header';
+import NewsLetter from '../components/NewsLetter';
+import Footer from '../components/Footer';
+import { useLoaderData } from 'react-router-dom';
+import MainBody from './components/MainBody';
+
 export default function MensPage() {
-	return <></>;
+	const data = useLoaderData();
+	return (
+		<div>
+			<Header />
+			<MainBody products={data} />
+			<NewsLetter />
+			<Footer />
+		</div>
+	);
 }
